2fa58dbaf1050c63bfbbaaf702158861ce0e52a1
judypvx <50930161+judypvx@users.noreply.github.com>
2025-04-08 22:46:51 +0200

    Create white.go

---
+++ normalize/white.go
@@ -0,0 +1,29 @@
+package normalizer
+
+import (
+    "strconv"
+)
+
+type RawWhiteItem struct {
+    ID       string
+    NameHash string
+    Price    string
+}
+
+type NormalizedItem struct {
+    Name     string
+    Price    float64
+    AssetID  string
+    Raw      any
+}
+
+func NormalizeWhite(p RawWhiteItem) NormalizedItem {
+    price, _ := strconv.ParseFloat(p.Price, 64)
+
+    return NormalizedItem{
+        Name:    p.NameHash,
+        Price:   price,
+        AssetID: p.ID, // 💡 тут ID == asset_id
+        Raw:     p,
+    }
+}
